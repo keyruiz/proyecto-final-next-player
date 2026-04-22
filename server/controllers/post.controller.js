800517494b7bb625ccf1903ac149d5fea433c394
keynis <keydejun@gmail.com>
2026-04-22 18:40:42 +0200

    Create post hecho

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -2,7 +2,7 @@ import { supabase} from '../supabase.js';
 
 export const getPosts = async (req, res) => {
   const { game } = req.query; 
-  
+
   try {
     let query = supabase
       .from('posts')
@@ -17,6 +17,40 @@ export const getPosts = async (req, res) => {
 
     if (error) throw new Error(error.message);
     res.json(data);
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+export const createPost = async (req, res) => {
+  const { user_id, game_id, title, description, role, rank } = req.body;
+
+  try {
+    // Verificamos si ya tiene uno
+    const { data: existingPost } = await supabase
+      .from('posts')
+      .select('id')
+      .eq('user_id', user_id)
+      .maybeSingle();
+      
+    if (existingPost) return res.status(400).json({ message: "Ya tienes un post activo" });
+
+    const { data, error } = await supabase
+      .from('posts')
+      .insert([{ 
+        user_id, 
+        game_id, 
+        title, 
+        description, 
+        role, 
+        rank,
+        is_active: true 
+      }])
+      .select();
+
+    if (error) throw error;
+    res.status(201).json(data);
 
   } catch (err) {
     res.status(500).json({ error: err.message });
